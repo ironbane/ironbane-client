a91eb85ff443e2f46156234bc5a5882d41896fce
Ben Sparks <bsparks42@gmail.com>
2014-10-02 13:39:38 -0700

    switch to individually wrapped ces nuggets, so that Entity can subclass Object3D again...

---
+++ src/core/ces/ces.js
@@ -0,0 +1,10 @@
+angular.module('ces', [
+    'ces.class',
+    'ces.component',
+    'ces.entity',
+    'ces.entitylist',
+    'ces.family',
+    'ces.signal',
+    'ces.system',
+    'ces.world'
+]);
---
+++ src/core/ces/entity.js
@@ -0,0 +1,59 @@
+angular.module('ces.entity', [
+    'three',
+    'ces.signal'
+])
+    .factory('Entity', function (THREE, Signal) {
+
+        var Entity = function () {
+            THREE.Object3D.call(this);
+
+            this._components = {};
+            this.onComponentAdded = new Signal();
+            this.onComponentRemoved = new Signal();
+        };
+
+        Entity.prototype = Object.create(THREE.Object3D.prototype);
+        Entity.prototype.constructor = Entity;
+
+        /**
+         * Check if this entity has a component by name.
+         * @public
+         * @param {String} componentName
+         * @return {Boolean}
+         */
+        Entity.prototype.hasComponent = function (componentName) {
+            return this._components['$' + componentName] !== undefined;
+        };
+
+        /**
+         * Get a component of this entity by name.
+         * @public
+         * @param {String} componentName
+         * @return {Component}
+         */
+        Entity.prototype.getComponent = function (componentName) {
+            return this._components['$' + componentName];
+        };
+
+        /**
+         * Add a component to this entity.
+         * @public
+         * @param {Component} component
+         */
+        Entity.prototype.addComponent = function (component) {
+            this._components['$' + component.name] = component;
+            this.onComponentAdded.emit(this, component.name);
+        };
+
+        /**
+         * Remove a component from this entity by name.
+         * @public
+         * @param {String} componentName
+         */
+        Entity.prototype.removeComponent = function (componentName) {
+            this._components['$' + componentName] = undefined;
+            this.onComponentRemoved.emit(this, componentName);
+        };
+
+        return Entity;
+    });
