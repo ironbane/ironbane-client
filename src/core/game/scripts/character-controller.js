fea23e52c223dfa5df0937400bd972d61119c02a
Nick Janssen <info@nickjanssen.com>
2014-11-01 18:32:28 +0100

    character-controller: use input vector

--- src/core/game/scripts/character-controller.js
+++ src/core/game/scripts/character-controller.js
@@ -1,5 +1,5 @@
-angular.module('game.scripts.character-controller', ['components.script'])
-    .run(function ($log, ScriptBank) {
+angular.module('game.scripts.character-controller', ['components.script', 'three'])
+    .run(function ($log, ScriptBank, THREE) {
         'use strict';
 
         var moveSpeed = 10;
@@ -11,11 +11,11 @@ angular.module('game.scripts.character-controller', ['components.script'])
 
             this.moveForward = false;
             this.moveBackward = false;
-            this.moveLeft = false;
-            this.moveRight = false;
-
             this.rotateLeft = false;
             this.rotateRight = false;
+
+            this.moveLeft = false;
+            this.moveRight = false;
         };
 
         CharacterControllerScript.prototype.update = function (dt, elapsed, timestamp) {
@@ -26,10 +26,10 @@ angular.module('game.scripts.character-controller', ['components.script'])
             // reset these every frame
             this.moveForward = false;
             this.moveBackward = false;
-            this.moveLeft = false;
-            this.moveRight = false;
             this.rotateLeft = false;
             this.rotateRight = false;
+            this.moveLeft = false;
+            this.moveRight = false;
 
             // virtual gamepad (touch ipad) controls
             if (leftStick) {
@@ -41,10 +41,10 @@ angular.module('game.scripts.character-controller', ['components.script'])
                 }
 
                 if (leftStick.delta.x < 0) {
-                    this.moveLeft = true;
+                    this.rotateLeft = true;
                 }
                 if (leftStick.delta.x > 0) {
-                    this.moveRight = true;
+                    this.rotateRight = true;
                 }
             }
 
@@ -58,45 +58,48 @@ angular.module('game.scripts.character-controller', ['components.script'])
             }
 
             if (input.keyboard.isDown(input.KEYS.A) || input.keyboard.isDown(input.KEYS.LEFT)) {
-                this.moveLeft = true;
-            }
-
-            if (input.keyboard.isDown(input.KEYS.D) || input.keyboard.isDown(input.KEYS.RIGHT)) {
-                this.moveRight = true;
-            }
-
-            if (input.keyboard.isDown(input.KEYS.Q)) {
                 this.rotateLeft = true;
             }
 
-            if (input.keyboard.isDown(input.KEYS.E)) {
+            if (input.keyboard.isDown(input.KEYS.D) || input.keyboard.isDown(input.KEYS.RIGHT)) {
                 this.rotateRight = true;
             }
 
+            if (input.keyboard.isDown(input.KEYS.Q)) {
+                this.moveLeft = true;
+            }
+
+            if (input.keyboard.isDown(input.KEYS.E)) {
+                this.moveRight = true;
+            }
+
+            var inputVector = new THREE.Vector3();
+
             // react to changes
             if (this.moveForward) {
-                this.entity.translateZ(-moveSpeed * dt);
+                inputVector.z -= 1;
             }
             if (this.moveBackward) {
-                this.entity.translateZ(moveSpeed * dt);
+                inputVector.z += 1;
             }
             if (this.moveLeft) {
-                this.entity.rotateY(rotateSpeed * dt);
+                inputVector.x -= 1;
             }
             if (this.moveRight) {
-                this.entity.rotateY(-rotateSpeed * dt);
+                inputVector.x += 1;
             }
+
+            // Make sure they can't gain extra speed if moving diagonally
+            inputVector.normalize();
+
+            this.entity.translateOnAxis(inputVector, moveSpeed * dt);
 
             if (this.rotateLeft) {
-                this.entity.translateX(-moveSpeed * dt);
+                this.entity.rotateY(rotateSpeed * dt);
             }
             if (this.rotateRight) {
-                this.entity.translateX(moveSpeed * dt);
+                this.entity.rotateY(-rotateSpeed * dt);
             }
-
-
-            // cameraComponent.camera.position.set(Math.cos(timestamp / 1000) * 10, 20, Math.sin(timestamp / 1000) * 15);
-            // cameraComponent.camera.lookAt(new THREE.Vector3(0, 0, 0));
         };
 
         ScriptBank.add('/scripts/built-in/character-controller.js', CharacterControllerScript);
